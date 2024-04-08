362f077810112ebc38af55319a24a05c09856144
LoDaa222 <112142384+LoDaa222@users.noreply.github.com>
2024-04-08 08:29:31 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+document.querySelector("#myCarousel > button.carousel-control-prev")
+
+
+
+window.onscroll = function()
+{myFunction()};
+
+ var header =
+ document.getElementById("myHeader");
+
+ var sticky = header.offsetTop;
+
+ function myFunction() {
+ if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+
+  }else {
+    header.classList.remove("sticky");
+  }
+ 
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelector("#myCarousel")
+
+document.querySelector("body > main > div:nth-child(2)")
+
+document.querySelector("body > div:nth-child(8)")
+
+
+document.querySelector("body > main > div.container.marketing > div:nth-child(1)")
+
+
+document.querySelector("#myCarousel")
+
+
+document.querySelector("body > main > div.container.marketing > div:nth-child(5)")
+
+
+document.querySelector("body > main > div.container.marketing > div:nth-child(7)")
+
+document.querySelector("body > main > div:nth-child(10)")
